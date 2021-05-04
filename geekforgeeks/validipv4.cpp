faefaa738ab5cea9bc927485805649dc84f8f83b
Dhruba Jyoti Chakraborty <61685945+dhrubajyoti89@users.noreply.github.com>
2021-05-04 23:46:23 +0530

    Create validipv4.cpp

---
+++ geekforgeeks/validipv4.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+ int isValid(string s);
+
+int main(){
+  int t;
+  cin>> t;
+  while(t--){
+    string s;
+    cin>>s;
+    cout<<isVaild(s)<<endl;
+  }
+  return 0;
+}
+
+int isValid(string str){
+int dotCount = 0;
+    for(int i = 0; i < str.size(); i++)
+    {
+        if(str[i] == '.')
+        {
+            dotCount++;
+        }
+    }
+    
+    if(dotCount != 3)
+    {
+        return false;
+    }
+    
+    stringstream ss(str);
+    string temp;
+    
+    while(getline(ss, temp, '.'))
+    {
+        if(temp.size() > 3 || temp.size() == 0)
+        {
+            return false;
+        }
+        
+        if(temp.size() > 1 and temp[0] == '0')
+        {
+            return false;
+        }
+        
+        int num = 0;
+        for(int i = 0; i < temp.size(); i++)
+        {
+            if(temp[i] >= '0' and temp[i] <= '9')
+            {
+                num = num * 10 + (temp[i] - '0');
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        if(temp.size() > 1 and num == 0)
+        {
+            return false;
+        }
+        
+        if(num >= 0 and num <= 255)
+        {
+            continue;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    return true;
+  
+}
